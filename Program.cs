308331b5bc35f02ec7f8d3edc661623f5baa9efc
kameffee <kameffee@gmail.com>
2022-01-28 00:48:32 +0900

    first commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System;
+using System.IO;
+using System.Text;
+using System.Collections;
+
+
+namespace Solver
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+
+            Console.WriteLine(Environment.CurrentDirectory);
+            Console.WriteLine(Environment.SystemDirectory);
+            using (var stream = new StreamReader("../../../word_list.txt", Encoding.UTF8))
+            {
+                string line;
+                while ((line = stream.ReadLine()) != null)
+                {
+                    if (line.Length == 5)
+                    {
+                        list.Add(line);
+                    }
+                }
+            }
+
+            foreach (var word in list)
+            {
+                Console.Write(word + ",");
+            }
+
+            Console.WriteLine("End");
+        }
+    }
+}
