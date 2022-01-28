ee9979e8e2d547fd69fa1abf2affe8ef0c542b73
kameffee <kameffee@gmail.com>
2022-01-29 08:53:46 +0900

    入力サイクルを作成

--- Program.cs
+++ Program.cs
@@ -16,24 +16,25 @@ class Program
         UserWordInput userWordInput = new UserWordInput(wordDictionary, logger);
         ResultOutput resultOutput = new ResultOutput();
 
-        logger.Log("Start");
-
         var wordList = wordDictionary.All();
-        
         // logger.Log(string.Join(',', wordList));
 
         Console.Write("\n");
 
-        var inputWord = userWordInput.WaitWordInput();
-        logger.Log("");
+        for (int i = 0; i < 5; i++)
+        {
+            var inputWord = userWordInput.WaitWordInput(i + 1);
+            logger.Log("");
 
-        var inputResult = userWordInput.WaitResultInput();
+            var inputResult = userWordInput.WaitResultInput();
 
-        // 確認
-        logger.Log($"--------");
-        resultOutput.Output(inputWord.ToCharArray(), inputResult.ToCharArray());
-        Console.Write("\n");
-        logger.Log($"--------");
+            // 確認
+            logger.Log($"[{i}]:");
+            resultOutput.Output(inputWord.ToCharArray(), inputResult.ToCharArray());
+            Console.Write("\n");
+            logger.Log($"--------");
+        }
+
 
         logger.Log("End");
     }
--- UserWordInput.cs
+++ UserWordInput.cs
@@ -14,13 +14,13 @@ public class UserWordInput
     /// <summary>
     /// 施行したワードの入力待ち
     /// </summary>
-    public string WaitWordInput()
+    public string WaitWordInput(int phase)
     {
         // 入力待ち
         string? input = "";
         while (string.IsNullOrEmpty(input) || input.Length != 5 || !ValidateWord(input))
         {
-            _logger.Log($"試行した5文字のワード入力してください。");
+            _logger.Log($"{phase}回目に試行した5文字のワード入力してください。");
             input = Console.ReadLine();
 
             if (string.IsNullOrEmpty(input) || input.Length != 5)
